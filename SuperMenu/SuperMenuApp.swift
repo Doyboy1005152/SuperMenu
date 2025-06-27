824c17066be5dff08409199c471885567da08645
Liam Reynolds <liamareynolds@icloud.com>
2025-06-26 21:46:59 -0400

    Initial Commit

---
+++ SuperMenu/SuperMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperMenuApp.swift
+//  SuperMenu
+//
+//  Created by Liam Reynolds on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
